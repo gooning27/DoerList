a8ff3a61b9dfaea9bd04252e6087b6bf80b04613
gooning27 <lavinaronsaludo@yahoo.com>
2024-11-24 14:41:50 +0800

    Update DailyTaskUI.cs

--- DoerList/DailyTaskUI.cs
+++ DoerList/DailyTaskUI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +12,71 @@ namespace DoerList
 {
     public partial class DailyTaskUI : Form
     {
+        private List<string> dailyTasks = new List<string>(); // List to store daily tasks
+
         public DailyTaskUI()
         {
             InitializeComponent();
+            LoadDailyTasks();
+        }
+
+        private void LoadDailyTasks()
+        {
+            foreach (var task in dailyTasks)
+            {
+                if (!listBoxTasks.Items.Contains(task))
+                {
+                    listBoxTasks.Items.Add(task);
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                if (chkDaily.Checked)
+                {
+                    if (!dailyTasks.Contains(txtTask.Text))
+                    {
+                        dailyTasks.Add(txtTask.Text); // Add to daily tasks list
+                        MessageBox.Show("Task added to daily tasks.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    listBoxTasks.Items.Add(txtTask.Text); // Add to normal list
+                }
+
+                txtTask.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedItem != null)
+            {
+                listBoxTasks.Items.Remove(listBoxTasks.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.", "Selection Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnRemoveAll_Click(object sender, EventArgs e)
+        {
+            listBoxTasks.Items.Clear();
+        }
+
+        private void btnReloadDailyTasks_Click(object sender, EventArgs e)
+        {
+            LoadDailyTasks();
         }
     }
 }
